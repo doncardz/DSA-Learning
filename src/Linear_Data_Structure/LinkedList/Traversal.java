451bd0aa80f016293c8139a0acc534e2022c2287
doncardz <carloventocilla11@gmail.com>
2026-09-14 18:22:47 +0800

    Traversal

--- src/Linear_Data_Structure/LinkedList/Traversal.java
+++ src/Linear_Data_Structure/LinkedList/Traversal.java
@@ -1,7 +1,31 @@
 package Linear_Data_Structure.LinkedList;
 
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
 public class Traversal {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Node head = new Node(10);
+        Node secondNode = new Node(20);
+        Node thirdNode = new Node(30);
+        Node lastNode = new Node(40);
+
+        head.next = secondNode;
+        secondNode.next = thirdNode;
+        thirdNode.next = lastNode;
+
+        Node current = head;
+
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
     }
 }
